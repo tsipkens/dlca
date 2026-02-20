d43fafd915bf86e8208d4c970e1cf905ce02664f
Timothy Sipkens <tsipkens@gmail.com>
2026-02-20 18:37:01 -0500

    Update README

--- main_dlca.py
+++ main_dlca.py
@@ -2,6 +2,7 @@
 import numpy as np
 import models
 
+# %%
 n_particles = np.random.randint(20, 51, 1)
 for ii in range(len(n_particles)):
     pos, dsu, box_size = models.run(n_particles[ii], seed_density=0.0008, f_xyz=True)
