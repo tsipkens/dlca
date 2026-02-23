9e245ba8d408a0f83f1b2e04c47cbf5fea0dbbd5
Timothy Sipkens <tsipkens@gmail.com>
2026-02-23 17:10:57 -0500

    Add a bunch more features to main script

--- main_dlca.py
+++ main_dlca.py
@@ -1,9 +1,38 @@
 
 import numpy as np
 import models
+import tools
 
 # %%
-n_particles = np.random.randint(20, 51, 1)
+
+pos = []
+
+n_particles = np.random.randint(20, 31, 5)
 for ii in range(len(n_particles)):
-    pos, dsu, box_size = models.run(n_particles[ii], seed_density=0.0008, f_xyz=2)
+    pos_ii, dsu, box_size = models.run(n_particles[ii], seed_density=0.0008, f_xyz=2)
+
+    pos.append(pos_ii)
+
+
+# %%
+
+pos_sc = []
+da = []
+dpp = []
+rho = []
+
+for ii in range(len(n_particles)):
+    sc = tools.scale_agg(pos[ii])
+
+    # Append outputs to respective lists.
+    pos_sc.append(sc[0])
+    dpp.append(sc[1] * 2)
+    da.append(sc[2])
+
+    rho.append(tools.rho_eff(pos[-1], da=da[-1], dpp=dpp[-1]))
+
+
+# %%
+
+img = tools.plot_projection(pos[-1], type='tem', tem_args={'noise_floor': 0.15})
 
